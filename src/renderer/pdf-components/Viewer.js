7a8eb1178c75cc6dc36f6e9a0e82e85430f6140f
David Stein <davidbstein@gmail.com>
2021-10-11 01:39:50 -0400

    broken loading animation.

--- src/renderer/pdf-components/Viewer.js
+++ src/renderer/pdf-components/Viewer.js
@@ -1,7 +1,23 @@
 import ReactDOM from "react-dom";
 import React, { Component } from "react";
 import "pdfjs-dist/web/pdf_viewer.css";
-import MouseFollower from "@/components/MouseFollower";
+import MouseFollower from "@/pdf-components/MouseFollower";
+
+class LoadingBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dots: 1,
+    };
+  }
+  render() {
+    return (
+      <div id="mainloadingdiv">
+        <div>loading...</div>
+      </div>
+    );
+  }
+}
 
 export default class Viewer extends Component {
   constructor(props) {
@@ -17,7 +33,9 @@ export default class Viewer extends Component {
   render() {
     return (
       <div id="Viewer">
-        <div id="pdfViewer"></div>
+        <div id="pdfViewer">
+          <LoadingBar />
+        </div>
         <MouseFollower />
       </div>
     );
